e1614ae60ecbd9b25352ceea5b19beb8499020b2
RenatoStanz <stanz@tutamail.com>
2023-11-06 00:02:21 -0300

    C10 - Hill Climb, Demo.py Fixed

--- hill_climbing-A1/demo.py
+++ hill_climbing-A1/demo.py
@@ -1,5 +1,5 @@
 from handler import *
-from hill_climbing import HillClimbing
+from HillClimbing import HillClimbing
 
 coordinates = get_coordinates()
 
@@ -14,35 +14,43 @@ for i in range(1):
     climb = HillClimbing(cities, cities_table)
     print("climb:", climb)
     print("\nOperator 1:")
-    print(climb.hill_climb())
+    climb.hill_climb()
+    print(climb)
     
     climb = HillClimbing(cities, cities_table)
     print("\nOperator 2:")
-    print(climb.hill_climb(is_first_oper=False))
+    climb.hill_climb(is_first_oper=False)
+    print(climb)
     
     climb = HillClimbing(cities, cities_table)
     print("\nRandom 1:")
-    print(climb.random_hill_climb())
+    climb.random_hill_climb()
+    print(climb)
     
     climb = HillClimbing(cities, cities_table)
     print("\nRandom 2:")
-    print(climb.random_hill_climb(is_first_oper=False))
+    climb.random_hill_climb(is_first_oper=False)
+    print(climb)
 
 
     print("\n\nInit 2\n")
 
     climb = HillClimbing(cities, cities_table, is_init_2=True)
     print("\nOperator 1:")
-    print(climb.hill_climb())
+    climb.hill_climb()
+    print(climb)
     
     climb = HillClimbing(cities, cities_table, is_init_2=True)
     print("\nOperator 2:")
-    print(climb.hill_climb(is_first_oper=False))
+    climb.hill_climb(is_first_oper=False)
+    print(climb)
     
     climb = HillClimbing(cities, cities_table, is_init_2=True)
     print("\nRandom 1:")
-    print(climb.random_hill_climb())
+    climb.random_hill_climb()
+    print(climb)
     
     climb = HillClimbing(cities, cities_table, is_init_2=True)
     print("\nRandom 2:")
-    print(climb.random_hill_climb(is_first_oper=False))
+    climb.random_hill_climb(is_first_oper=False)
+    print(climb)
